2e983d74cd66507aa902ede579338e7b3b1ff95e
Greg Martz <gmartz@costco.com>
2019-05-23 12:35:55 -0700

    initial

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,29 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$version = '18_2_6'
+$fileName = "smartgit-win-$version.zip"
+$packageArgs = @{
+    packageName    = 'SmartGit'
+    fileType       = 'exe'
+    softwareName   = 'SmartGit'
+    unzipLocation  = Get-PackageCacheLocation
+
+    checksum       = '6F75442ACB7F2B6617111A8642357A1C3E9B2BB4E42714D7845CB0996D504DF0'
+    checksumType   = 'sha256'
+    url            = "https://www.syntevo.com/downloads/smartgit/$fileName"
+
+    silentArgs     = '/SILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+    validExitCodes = @(0)
+}
+
+Write-Verbose "Testing the default url location."
+if ((Get-WebHeaders $packageArgs.url).'Content-Type' -match "^text/html" ) {
+  Write-Warning "File not found, trying to find it in the archive."
+  $packageArgs.url = "https://www.syntevo.com/downloads/smartgit/archive/$fileName"
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+$packageArgs.file = (Join-Path $packageArgs.unzipLocation (Get-ChildItem $packageArgs.unzipLocation -Filter '*.exe' | select -first 1))
+
+Install-ChocolateyInstallPackage @packageArgs
